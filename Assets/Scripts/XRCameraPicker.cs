de232e8546181db522465f6ec46d79c164f5e949
rafek <rafal.legiedz@gmail.com>
2019-11-15 13:31:05 +0100

    Initial project setup.

---
+++ Assets/Scripts/XRCameraPicker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class XRCameraPicker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject HololensCamera;
+
+    [SerializeField]
+    private GameObject ARFoundationCamera;
+
+    [SerializeField]
+    private GameObject EditorCamera;
+
+    private void Awake()
+    {
+        var targetCamera = EditorCamera;
+
+#if UNITY_WSA
+        targetCamera = HololensCamera;
+#elif UNITY_ANDROID || UNITY_IOS
+        targetCamera = ARFoundationCamera;
+#elif !UNITY_EDITOR
+        Debug.LogError("Unexpected platform for XRCameraPicker.");
+#endif
+
+        Instantiate(targetCamera);
+    }
+}
