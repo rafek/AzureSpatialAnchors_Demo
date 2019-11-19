fe467286514bdfb631b1263f954d13f528d52468
rafek <rafal.legiedz@gmail.com>
2019-11-19 13:03:43 +0100

    Added object spwaning and local anchoring.

---
+++ Assets/Scripts/InputInteraction.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+#if WINDOWS_UWP || UNITY_WSA
+using UnityEngine.XR.WSA.Input;
+#endif
+
+public class InputInteraction : MonoBehaviour
+{
+    public event Action<Vector3> OnSelectObjectInteraction;
+
+#if UNITY_ANDROID || UNITY_IOS
+    private ARRaycastManager arRaycastManager;
+#endif
+
+    private void Start()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+
+        if (arRaycastManager == null)
+        {
+            Debug.Log("Missing ARRaycastManager in the scene.");
+        }
+#endif
+
+#if WINDOWS_UWP || UNITY_WSA
+        InteractionManager.InteractionSourcePressed += InteractionManager_InteractionSourcePressed;
+#endif
+    }
+
+    private void Update()
+    {
+        TriggerInteractions();
+    }
+
+    private void OnDestroy()
+    {
+#if WINDOWS_UWP || UNITY_WSA
+        InteractionManager.InteractionSourcePressed -= InteractionManager_InteractionSourcePressed;
+#endif
+    }
+
+#if WINDOWS_UWP || UNITY_WSA
+    private void InteractionManager_InteractionSourcePressed(InteractionSourcePressedEventArgs obj)
+    {
+        if (obj.pressType == InteractionSourcePressType.Select)
+        {
+            OnSelectInteraction();
+        }
+    }
+
+    private void OnSelectInteraction()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            OnSelectObjectInteraction?.Invoke(hit.point);
+        }
+    }
+#endif
+
+    private void TriggerInteractions()
+    {
+        // TODO: gaze interactions
+
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            // TODO: ignore UI events
+
+            OnTouchInteraction(touch);
+        }
+    }
+
+    private void OnTouchInteraction(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Ended)
+        {
+            OnTouchInteractionEnded(touch);
+        }
+    }
+
+    private void OnTouchInteractionEnded(Touch touch)
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        var arRaycastHits = new List<ARRaycastHit>();
+
+        if (arRaycastManager.Raycast(touch.position, arRaycastHits) && arRaycastHits.Count > 0)
+        {
+            var hit = arRaycastHits[0];
+
+            OnSelectObjectInteraction?.Invoke(hit.pose.position);
+        }
+#elif WINDOWS_UWP || UNITY_WSA
+#endif
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.SpatialAnchors.Unity;
+using System;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public event Action<GameObject> OnObjectSpawned;
+
+    [SerializeField]
+    private GameObject anchoredObjectPrefab;
+
+    private InputInteraction inputInteraction;
+
+    private void Awake()
+    {
+        inputInteraction = GetComponent<InputInteraction>();
+
+        inputInteraction.OnSelectObjectInteraction += InputInteraction_OnSelectObjectInteraction;
+    }
+
+    private void OnDestroy()
+    {
+        if (inputInteraction != null)
+        {
+            inputInteraction.OnSelectObjectInteraction -= InputInteraction_OnSelectObjectInteraction;
+        }
+    }
+
+    private void InputInteraction_OnSelectObjectInteraction(Vector3 position)
+    {
+        var spawnedObject = SpawnAt(position);
+
+        OnObjectSpawned?.Invoke(spawnedObject);
+    }
+
+    private GameObject SpawnAt(Vector3 position)
+    {
+        var newAnchoredGameObject = Instantiate(anchoredObjectPrefab, position, Quaternion.identity);
+
+        newAnchoredGameObject.CreateNativeAnchor();
+        newAnchoredGameObject.AddComponent<CloudNativeAnchor>();
+
+        return newAnchoredGameObject;
+    }
+}
