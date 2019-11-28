16d153cecb7276d14abe405ac7a878fb8d76b824
rafek <rafal.legiedz@gmail.com>
2019-11-28 09:36:30 +0100

    Sending local anchors to the cloud. Added TextMesh Pro.

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Azure.SpatialAnchors.Unity;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    [SerializeField]
+    private SpatialAnchorManager spatialAnchorManager;
+
+    private ObjectSpawner objectSpawner;
+
+    private void Awake()
+    {
+        objectSpawner = GetComponent<ObjectSpawner>();
+
+        objectSpawner.OnObjectSpawned += ObjectSpawner_OnObjectSpawned;
+    }
+
+    private async void Start()
+    {
+        try
+        {
+            await spatialAnchorManager.CreateSessionAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Error: {ex.Message}");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (objectSpawner != null)
+        {
+            objectSpawner.OnObjectSpawned -= ObjectSpawner_OnObjectSpawned;
+        }
+
+        if (spatialAnchorManager != null)
+        {
+            spatialAnchorManager.DestroySession();
+        }
+    }
+
+    private async void ObjectSpawner_OnObjectSpawned(GameObject spawnedObject)
+    {
+        await OnObjectSpawned(spawnedObject);   
+    }
+
+    private async Task OnObjectSpawned(GameObject spawnedObject)
+    {
+        var cloudNativeAnchor = spawnedObject.GetComponent<CloudNativeAnchor>();
+
+        if (cloudNativeAnchor.CloudAnchor == null)
+        {
+            cloudNativeAnchor.NativeToCloud();
+        }
+
+        var cloudSpatialAnchor = cloudNativeAnchor.CloudAnchor;
+
+        // TODO: add expirtaion, add properties
+
+        while (!spatialAnchorManager.IsReadyForCreate)
+        {
+            await Task.Delay(300);
+
+            // TODO: display RecommendedForCreateProgress feedback
+
+            var createProgress = spatialAnchorManager.SessionStatus.RecommendedForCreateProgress;
+
+            Debug.Log($"Create Progress: {createProgress}");
+        }
+
+        try
+        {
+            await spatialAnchorManager.CreateAnchorAsync(cloudSpatialAnchor);
+
+
+            // TODO: save that anchor to sharing service if successful
+
+            Debug.Log("Anchor saved: " + cloudSpatialAnchor.Identifier);
+        }
+        catch (Exception ex)
+        {
+            // TODO: handle exception
+        }
+    }
+}
